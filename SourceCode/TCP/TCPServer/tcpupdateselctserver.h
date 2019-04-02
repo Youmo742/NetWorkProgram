82554bee05f67f0bab597d96f2e7d83a3a51068a
Youmo742 <moyou742@163.com>
2019-04-02 19:54:49 +0800

    以类的形式包装服务端

---
+++ SourceCode/TCP/TCPServer/tcpupdateselctserver.h
@@ -0,0 +1,114 @@
+#ifndef TCPUPDATESELCTSERVER_H
+#define TCPUPDATESELCTSERVER_H
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define PORT 10001
+#define RT_ERR (-1)
+#define RT_OK 0
+#define SERVERIP "127.0.0.1"
+#define LISTEN_QUEUE 10
+#define BUFFER_SIZE 1024
+//服务器使用select模型
+int test()
+{
+    std::vector<int>v;
+    int listenfd, connsockfd;
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(listenfd < 0)
+    {
+            fprintf(stderr, "socket function failed.\n");
+            exit(RT_ERR);
+    }
+
+    struct  sockaddr_in serveraddr, clientaddr;
+    bzero(&serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(PORT);
+    serveraddr.sin_addr.s_addr = inet_addr(SERVERIP);
+
+    unsigned int client_len = sizeof(struct sockaddr_in);
+    if(bind(listenfd, (struct sockaddr *)&serveraddr, sizeof(struct sockaddr_in)) < 0)
+    {
+            fprintf(stderr, "bind function failed.\n");
+            close(listenfd);
+            exit(RT_ERR);
+    }
+
+    if(listen(listenfd,LISTEN_QUEUE) < 0)
+    {
+            fprintf(stderr, "listen function failed.\n");
+            close(listenfd);
+            exit(RT_ERR);
+    }
+    fprintf(stdout, "The server IP is %s, listen on port: %d\n", inet_ntoa(serveraddr.sin_addr), ntohs(serveraddr.sin_port));
+
+    timeval t={1,0};
+    fd_set readfdset;
+    while(1)
+    {
+        FD_ZERO(&readfdset);
+        for(auto fd:v)
+        {
+            FD_SET(fd, &readfdset);
+        }
+        FD_SET(listenfd, &readfdset);
+
+        if(!(select(FD_SETSIZE, &readfdset, NULL, NULL, NULL) > 0))
+        {
+            fprintf(stderr, "select function failed.\n");
+            close(listenfd);
+            exit(RT_ERR);
+        }
+        if(FD_ISSET(listenfd, &readfdset))
+        {
+//            fprintf(stdout, "fd is %d, listenfd is %d\n", fd, listenfd);
+            std::cout<<"new client"<<std::endl;
+            if((connsockfd = accept(listenfd, (struct sockaddr*)&clientaddr, &client_len)) < 0)
+            {
+                fprintf(stderr, "accept function failed.\n");
+                exit(RT_ERR);
+            }
+            for(auto t:v)
+            {
+                send(t,"new client connect",sizeof("new client connect"),0);
+            }
+            v.push_back(connsockfd);
+            fprintf(stdout, "It is a new session from IP:%s port:%d\n",inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+        }
+         for(int fd=listenfd+1;fd<FD_SETSIZE;fd++)
+         {
+             if(FD_ISSET(fd, &readfdset))
+             {
+                 char readbuf[BUFFER_SIZE]={'\0'};
+                 if(recv(fd, readbuf, BUFFER_SIZE, 0) > 0)
+                 {
+                     fprintf(stdout, "recv message: %s\n", readbuf);
+                 }
+                 else
+                 {
+                     FD_CLR(fd,&readfdset);
+                     auto t=std::find(v.begin(),v.end(),fd);
+                     v.erase(t);
+                     close(fd);
+                     fprintf(stdout, "client socket %d close\n", fd);
+                 }
+             }
+        }//end of for
+    }//end of while
+    close(listenfd);
+    return 0;
+}
+
+#endif // TCPUPDATESELCTSERVER_H
